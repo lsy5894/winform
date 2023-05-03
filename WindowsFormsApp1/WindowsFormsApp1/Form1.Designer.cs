0c10fdb3e4622162b38b45d3879af7defdd30fa2
lsy5894 <sinvy501@naver.com>
2023-05-03 23:33:26 +0900

    파일추가

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.chart1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(14, 26);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(887, 521);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.LegendStyle = System.Windows.Forms.DataVisualization.Charting.LegendStyle.Column;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(3, 4);
+            this.chart1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(881, 252);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseMove);
+            // 
+            // dataGridView1
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 264);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(881, 253);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, -2);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(115, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dateTimePicker2.Location = new System.Drawing.Point(196, 1);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 25);
+            this.dateTimePicker2.TabIndex = 3;
+            this.dateTimePicker2.Visible = false;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.CustomFormat = "yyyy-MM-dd HH:mm:ss ";
+            this.dateTimePicker3.Location = new System.Drawing.Point(402, 1);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(200, 25);
+            this.dateTimePicker3.TabIndex = 4;
+            this.dateTimePicker3.Visible = false;
+            this.dateTimePicker3.ValueChanged += new System.EventHandler(this.dateTimePicker3_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 562);
+            this.Controls.Add(this.dateTimePicker3);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataTable table = new DataTable();
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+           // DataTable table = new DataTable();
+
+            // column을 추가합니다.
+            table.Columns.Add("일시", typeof(string));
+            table.Columns.Add("julainday", typeof(string));
+            table.Columns.Add("조위", typeof(string));
+
+
+       
+            // 각각의 행에 내용을 입력합니다.
+            table.Rows.Add("2023-01-01 00:00", "1", "0.20");
+            table.Rows.Add("2023-01-01 00:10", "2", "0.17");
+            table.Rows.Add("2023-02-01 00:20", "3", "-0.15");
+            table.Rows.Add("2023-02-01 00:30", "5", "-0.12");
+            table.Rows.Add("2023-03-01 00:40", "1", "0.12");
+            table.Rows.Add("2023-04-01 00:50", "3", "-0.10");
+            table.Rows.Add("2023-05-01 01:00", "4", "0.15");
+            table.Rows.Add("2023-06-01 01:10", "2", "0.17");
+            table.Rows.Add("2023-06-01 01:20", "3", "-0.19");
+            table.Rows.Add("2023-07-01 03:20", "5", "-0.19");
+            table.Rows.Add("2023-08-01 05:20", "2", "0.18");
+            table.Rows.Add("2023-09-01 06:20", "1", "0.14");
+            table.Rows.Add("2023-09-01 07:10", "2", "0.19");
+            table.Rows.Add("2023-010-01 07:20", "3", "0.15");
+            table.Rows.Add("2023-011-01 08:20", "4", "0.23");
+            table.Rows.Add("2023-12-01 09:20", "2", "0.25");
+            // 값들이 입력된 테이블을 DataGridView에 입력합니다.
+            dataGridView1.DataSource = table;
+
+            dataGridView1.Columns[0].Frozen = true;
+            dataGridView1.Columns[0].DefaultCellStyle.Format = "yyyy-MM-dd HH:mm";
+            //dataGridView1.Columns[0].DefaultCellStyle.Format = "yyyy-MM-dd HH:00";
+
+
+            chart1.ChartAreas[0].CursorX.IsUserSelectionEnabled = true; // // X-축 zoom : 속성 설정. 마우스 클릭 & 드래그       
+            chart1.Series[0] = new Series();
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            chart1.Series[0].XValueMember = dataGridView1.Columns[0].DataPropertyName;
+            chart1.Series[0].YValueMembers = dataGridView1.Columns[1].DataPropertyName;
+
+            chart1.DataSource = dataGridView1.DataSource;
+            chart1.ChartAreas[0].AxisX.LabelStyle.Angle = -45;// x축 45도 회전
+            chart1.Series[0].Name = "조위";
+
+
+        }
+        DateTimePicker dateTimePicker1;
+
+        private void DateTimePickerChange(object sender, EventArgs e)
+        {
+            dataGridView1.CurrentCell.Value = dateTimePicker1.Text.ToString();
+            MessageBox.Show(string.Format("Date changed to {0}", dateTimePicker1.Text.ToString()));
+        }
+
+        private void DateTimePickerClose(object sender, EventArgs e)
+        {
+            dateTimePicker1.Visible = false;
+        }
+
+  
+
+        private void dataGridView1_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            // Check the cell clicked is not the column header cell
+            if (e.RowIndex != -1) //header 아닌지 check
+            {
+                // Apply on column index in which you want to display DatetimePicker.            
+                if (e.ColumnIndex == 0)
+                {
+                    dateTimePicker1 = new DateTimePicker();  // dateTimePicker 초기화                                                      
+                    dataGridView1.Controls.Add(dateTimePicker1); //dataGridView1에 dateTimePicker1 추가 
+
+                    // Setting the format i.e. mm/dd/yyyy)
+                    dateTimePicker1.Format = DateTimePickerFormat.Short;
+
+
+                    // dateTimeOffsetEdit1.CustomDisplayText = "yyyy-MM-dd HH:mm:ss ";
+
+                    dateTimePicker1.Format = DateTimePickerFormat.Custom;
+                    dateTimePicker1.CustomFormat = "yyyy-MM-dd HH:mm:ss "; //2018-01-01 12:30
+                                                                           // dateTimePicker1.ShowUpDown = true;
+
+                    // Create retangular area that represents the display area for a cell.
+                    Rectangle oRectangle = dataGridView1.GetCellDisplayRectangle(e.ColumnIndex, e.RowIndex, true);
+
+                    dateTimePicker1.Size = new Size(oRectangle.Width, oRectangle.Height);
+                    // Setting location for dateTimePicker1.
+                    dateTimePicker1.Location = new Point(oRectangle.X, oRectangle.Y);
+                    // An event attached to dateTimePicker1 which is fired when any date is selected.
+                    dateTimePicker1.TextChanged += new EventHandler(DateTimePickerChange);
+                    // An event attached to dateTimePicker1 which is fired when DateTimeControl is closed.
+                    dateTimePicker1.CloseUp += new EventHandler(DateTimePickerClose);
+
+                }
+            }
+
+        }
+
+        // Point? prevPosition = null;
+        // ToolTip tooltip = new ToolTip();
+
+        Point? prevPosition = null;
+        ToolTip tooltip = new ToolTip();
+
+        
+         private void chart1_MouseMove(object sender, MouseEventArgs e)
+         {
+             var pos = e.Location;
+             if (prevPosition.HasValue && pos == prevPosition.Value)
+                 return;
+             tooltip.RemoveAll();
+             prevPosition = pos;
+
+             var results = chart1.HitTest(pos.X, pos.Y, false, ChartElementType.DataPoint); // set ChartElementType.PlottingArea for full area, not only DataPoints
+             foreach (var result in results)
+             {
+                 if (result.ChartElementType == ChartElementType.DataPoint) // set ChartElementType.PlottingArea for full area, not only DataPoints
+                 {
+                    var prop = result.Object as DataPoint;
+
+                    var yVal = result.ChartArea.AxisY.PixelPositionToValue(pos.Y);
+                    // var xVal = result.ChartArea.AxisY.PixelPositionToValue(pos.X);
+
+                    tooltip.Show(((int)yVal).ToString(), chart1, pos.X, pos.Y - 15);
+                    tooltip.Show("X=" + prop.XValue + ", Y=" + prop.YValues[0], this.chart1,
+                                           pos.X, pos.Y - 15);
+                }
+             }
+         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //DateTimePicker dateTimePicker2 = new DateTimePicker();
+            //DateTimePicker dateTimePicker3 = new DateTimePicker();
+
+            dateTimePicker2.Visible = true;
+            dateTimePicker3.Visible = true;
+
+          
+
+            
+
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+                       
+           
+          
+        }
+
+        private void dateTimePicker3_ValueChanged(object sender, EventArgs e)
+        {
+            table.DefaultView.RowFilter = "일시 >= '" + dateTimePicker2.Value.Date + "' and  일시 <= '" +
+            dateTimePicker3.Value.Date + "'";
+
+
+            // dataGridView1.DataSource = table;
+            //chart1.DataSource = table;
+
+
+            chart1.Series[0].XValueType = ChartValueType.DateTime;
+            chart1.ChartAreas[0].AxisX.LabelStyle.Format = "yyyy-MM-dd HH:mm";
+
+            //chart1.DataSource = dataGridView1.DataSource;
+            //xyDiagram.AxisY.WholeRange.SetMinMaxValues(-1, 1); Y축범위 설정
+            DateTime atime = dateTimePicker2.Value.Date;
+            DateTime btime = dateTimePicker3.Value.Date;
+             chart1.ChartAreas[0].AxisX.Minimum = atime.ToOADate();
+            chart1.ChartAreas[0].AxisX.Maximum = btime.ToOADate();
+
+        }
+    }
+}
